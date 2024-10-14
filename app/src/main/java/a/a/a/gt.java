b75a71ab450cc69ea8f4d91979c42ae6273846ac
trindadedev13 <devsuay@gmail.com>
2024-10-14 01:21:44 -0300

    fixes

--- app/src/main/java/a/a/a/gt.java
+++ app/src/main/java/a/a/a/gt.java
@@ -17,6 +17,8 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.card.MaterialCardView;
+import com.google.android.material.color.MaterialColors;
 import com.sketchware.remod.R;
 
 import java.util.ArrayList;
@@ -126,7 +128,7 @@ public class gt extends LinearLayout {
     private void initialize(Activity activity) {
         wB.a(activity, this, R.layout.var_type_spinner);
         tvPreview = findViewById(R.id.tv_preview);
-        LinearLayout spinnerDialogLayout = (LinearLayout) wB.a(activity, R.layout.var_type_spinner_dialog);
+        MaterialCardView spinnerDialogLayout = (LinearLayout) wB.a(activity, R.layout.var_type_spinner_dialog);
         RecyclerView varTypeCategory = spinnerDialogLayout.findViewById(R.id.var_type_category);
         RecyclerView varTypeList = spinnerDialogLayout.findViewById(R.id.var_type_list);
         ((TextView) spinnerDialogLayout.findViewById(R.id.tv_title)).setText(xB.b().a(activity, R.string.logic_editor_more_block_title_add_variable_type));
@@ -233,9 +235,9 @@ public class gt extends LinearLayout {
             }
 
             if (layoutPosition == position) {
-                viewHolder.container.setBackgroundColor(getResources().getColor(com.google.android.material.R.attr.colorSurfaceContainerLow));
+                viewHolder.container.setBackgroundColor(MaterialColors.getColor(this, com.google.android.material.R.attr.colorSurfaceContainerLow));
             } else {
-                viewHolder.container.setBackgroundColor(getResources().getColor(com.google.android.material.R.attr.colorSurface));
+                viewHolder.container.setBackgroundColor(MaterialColors.getColor(this, com.google.android.material.R.attr.colorSurface));
             }
 
         }
